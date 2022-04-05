ef133ac12cd77ea919dc7cbc812d70c0c70a91bf
soniarez <101676781+soniarez@users.noreply.github.com>
2022-04-05 11:45:05 -0400

    modal box y separar cada 4 digitos

--- src/index.js
+++ src/index.js
@@ -1,99 +1,97 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 document.getElementById("pay").onclick = payment;
 
-function payment()  {
+function payment() {
+  //NO PERMITE CAMPOS VACÍOS
+  let errorCount = 0;
 
-//NO PERMITE CAMPOS VACÍOS
-    let errorCount = 0;
+  let errorOne = document.getElementById("error1");
+  if (document.getElementById("name").value == "") {
+    document.getElementById("name").style.borderColor = "red";
+    errorOne.textContent = "Ingresa tu nombre";
+    errorCount++;
+  } else {
+    errorOne.textContent = "";
+    document.getElementById("name").style.borderColor = "";
+  }
 
-    let errorOne = document.getElementById("error1");
-    if (document.getElementById("name").value == "") {
-       document.getElementById("name").style.borderColor = "red";
-       errorOne.textContent = "Ingresa tu nombre";
-       errorCount++;
-    } else {
-        errorOne.textContent = "";
-        document.getElementById("name").style.borderColor = "";
-    }
-    
-    let errorTwo = document.getElementById("error2");
-    if (document.getElementById("lastname").value == "") {
-       document.getElementById("lastname").style.borderColor = "red";
-       errorTwo.textContent = "Ingresa tu apellido";
-       errorCount++;
-    } else {
-        errorTwo.textContent = "";
-        document.getElementById("lastname").style.borderColor = "";
-    }
+  let errorTwo = document.getElementById("error2");
+  if (document.getElementById("lastname").value == "") {
+    document.getElementById("lastname").style.borderColor = "red";
+    errorTwo.textContent = "Ingresa tu apellido";
+    errorCount++;
+  } else {
+    errorTwo.textContent = "";
+    document.getElementById("lastname").style.borderColor = "";
+  }
 
-    let errorThree = document.getElementById("error3");
-    if (document.getElementById("card-number").value == "") {
-        document.getElementById("card-number").style.borderColor = "red";
-        errorThree.textContent = "Ingresa tu número de tarjeta";
-        errorCount++;
-     } else {
-        errorThree.textContent = "";
-        document.getElementById("card-number").style.borderColor = "";
-     }
+  let errorThree = document.getElementById("error3");
+  if (document.getElementById("card-number").value == "") {
+    document.getElementById("card-number").style.borderColor = "red";
+    errorThree.textContent = "Ingresa tu número de tarjeta";
+    errorCount++;
+  } else {
+    errorThree.textContent = "";
+    document.getElementById("card-number").style.borderColor = "";
+  }
 
-    let errorFour = document.getElementById("error4");
-    if (document.getElementById("expiration-date").value == "") {
-        document.getElementById("expiration-date").style.borderColor = "red";
-        errorFour.textContent = "Campo requerido";
-        errorCount++; 
-    } else {
-        errorFour.textContent = "";
-        document.getElementById("expiration-date").style.borderColor = "";
-    }
+  let errorFour = document.getElementById("error4");
+  if (document.getElementById("expiration-date").value == "") {
+    document.getElementById("expiration-date").style.borderColor = "red";
+    errorFour.textContent = "Campo requerido";
+    errorCount++;
+  } else {
+    errorFour.textContent = "";
+    document.getElementById("expiration-date").style.borderColor = "";
+  }
 
-    let errorFive = document.getElementById("error5");
-    if (document.getElementById("cvv").value == "") {
-        document.getElementById("cvv").style.borderColor = "red";
-        errorFive.textContent = "Campo requerido";
-        errorCount++; 
-    } else {
-        errorFive.textContent = "";
-        document.getElementById("cvv").style.borderColor = "";
-    }
+  let errorFive = document.getElementById("error5");
+  if (document.getElementById("cvv").value == "") {
+    document.getElementById("cvv").style.borderColor = "red";
+    errorFive.textContent = "Campo requerido";
+    errorCount++;
+  } else {
+    errorFive.textContent = "";
+    document.getElementById("cvv").style.borderColor = "";
+  }
 
-    if (errorCount > 0) {
-        return false;
-    }
+  if (errorCount > 0) {
+    return false;
+  }
 
+  //FUNCIONES DESDE VALIDATOR
 
-//FUNCIONES DESDE VALIDATOR   
+  let input = document.getElementById("card-number").value;
+  let userInput = input.replace(/\s/g, ""); //Removiendo en input los espacios vacíos que quedan al crear espacio cada 4 dígitos
 
-let input = document.getElementById("card-number").value;
-document.getElementById("card-number").value = validator.maskify(input);
-validator.stringLenght(input); //esta es la estructura para objetos
-validator.isANumber(input);
-validator.luhnValidation(input);
-validator.isValid(input);
+  document.getElementById("card-number").value = validator.maskify(userInput);
+  validator.stringLenght(userInput);
+  validator.isANumber(userInput);
+  validator.luhnValidation(userInput);
+  validator.isValid(userInput);
 
-let verification = validator.isValid(input);
-if (verification === true) {
-    /* swal(
-        text: "¡Pago exitoso!",
-        icon: "success",
-      }); */
-      alert("¡Bien, Pago exitoso!");
-      //return true;
-} else {
-   /* swal({
-        text: "Tarjeta no válida, revisa tus datos.",
-        icon: "error",
-      }); */
-    alert("Tarjeta no válida, revisa tus datos.");
-    //return false;
-} 
-
-//WINDOW RELOAD
-setTimeout(() => {
-    window.location.reload(true);
-}, 10000);
-242  
-
-//........
+  let verification = validator.isValid(userInput);
+  if (verification === true) {
+    document.getElementById("modal-yes").style.display = "block";
+    document.getElementById("close_btn-yes").onclick = function(){
+    document.getElementById("modal-yes").style.display = 'none'; 
+  } 
+  } else {
+    document.getElementById("modal-no").style.display = "block";
+    document.getElementById("close_btn-no").onclick = function(){
+    document.getElementById("modal-no").style.display = 'none';
+  };
+  }
 
 }
+
+//ESPACIO CADA 4 DÍGITOS
+document.getElementById("card-number").addEventListener("keyup", (e) => {
+  let valorInput = e.target.value;
+  document.getElementById("card-number").value = valorInput
+    .replace(/\s/g, "")
+    .replace(/\D/g, "")
+    .replace(/([0-9]{4})/g, "$1 ")
+    .trim();
+});
